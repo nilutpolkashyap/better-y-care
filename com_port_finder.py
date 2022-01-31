c846f4b8bb7f07084a97343409999416ca556ae0
Nilutpol Kashyap <47447024+nilutpolkashyap@users.noreply.github.com>
2022-01-31 23:58:14 +0530

    Update com_port_finder.py

--- com_port_finder.py
+++ com_port_finder.py
@@ -1,4 +1,8 @@
 import serial.tools.list_ports
 ports = list(serial.tools.list_ports.comports())
+port_list = []
 for p in ports:
-    print p
+    # print(p[0])
+    port_list.append(p[0])
+
+print(port_list)
